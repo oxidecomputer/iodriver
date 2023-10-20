a14990ff1b77edb5b5e0855c2c32001ca3c76225
iliana etaoin <iliana@oxide.computer>
2023-10-20 15:48:18 -0700

    implement `disk_format`

--- nixos/nixos-containers.nix
+++ nixos/nixos-containers.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   containers = builtins.mapAttrs
     (name: options: {
       config = { config, ... }: {
@@ -6,6 +6,12 @@
         system.stateVersion = config.system.nixos.version;
       };
       ephemeral = true;
+      bindMounts = lib.mkIf (options.disk_format != "block") {
+        "/" = { hostPath = "/iodriver"; isReadOnly = false; };
+      };
+      allowedDevices = lib.mkIf (options.disk_format == "block") [
+        { node = "/dev/cobblestone"; modifier = "rwm"; }
+      ];
     })
     config.iodriver.jobs.nixos-container;
 }
