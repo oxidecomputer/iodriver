beeadc8c9201a277b568ba8f1e710b9224579cde
iliana etaoin <iliana@oxide.computer>
2023-10-20 17:50:21 -0700

    little bit of module refactoring

--- nixos/default.nix
+++ nixos/default.nix
@@ -8,6 +8,7 @@ in
     ./jobs.nix
     ./nixos-containers.nix
     ./options.nix
+    ./vm.nix
   ];
 
   networking.hostName = "iodriver";
@@ -26,38 +27,6 @@ in
   isoImage.makeEfiBootable = true;
   isoImage.makeUsbBootable = true;
   isoImage.squashfsCompression = "zstd -Xcompression-level 3";
-
-  virtualisation.vmVariant = {
-    virtualisation.graphics = false;
-    virtualisation.memorySize = 4096;
-    virtualisation.useDefaultFilesystems = false;
-    virtualisation.fileSystems."/" = {
-      fsType = "tmpfs";
-      options = [ "mode=0755" ];
-    };
-
-    # Have `qemu-vm.nix` generate us an empty disk image, and use it as
-    # /dev/cobblestone.
-    virtualisation.diskImage = "./iodriver.qcow2";
-    virtualisation.diskSize = 128 * 1024;
-    iodriver.cobblestone = "vda";
-
-    # `installer/cd-dvd/iso-image.nix` sets a boot.postBootCommands which
-    # imports /nix/store/nix-path-registration, which does not exist in the 9p-
-    # based /nix/store, and then runs a nix-env command that attempts to realise
-    # a store path, which hits the network and fails. This is copied from
-    # `virtualisation/qemu-vm.nix` but with extra `lib.mkForce`.
-    boot.postBootCommands = lib.mkForce ''
-      if [[ "$(cat /proc/cmdline)" =~ regInfo=([^ ]*) ]]; then
-        ${config.nix.package.out}/bin/nix-store --load-db < ''${BASH_REMATCH[1]}
-      fi
-
-      # `qemu-vm.nix` does not wipe this device between runs, so ensure it's
-      # ready for use. (We could instead use `virtualisation.emptyDiskImages`
-      # but that puts them in $TMPDIR and the UX there is kinda bad.)
-      ${pkgs.util-linux}/bin/wipefs --all --quiet /dev/vda
-    '';
-  };
 
   boot.loader.timeout = lib.mkForce 3;
   boot.kernelParams = [ "console=tty0" ];
---
+++ nixos/vm.nix
@@ -0,0 +1,33 @@
+{
+  virtualisation.vmVariant = { config, lib, pkgs, ... }: {
+    virtualisation.graphics = false;
+    virtualisation.memorySize = 4096;
+    virtualisation.useDefaultFilesystems = false;
+    virtualisation.fileSystems."/" = {
+      fsType = "tmpfs";
+      options = [ "mode=0755" ];
+    };
+
+    # Have `qemu-vm.nix` generate us an empty disk image, and use it as
+    # /dev/cobblestone.
+    virtualisation.diskImage = "./iodriver.qcow2";
+    virtualisation.diskSize = 128 * 1024;
+    iodriver.cobblestone = "vda";
+
+    # `installer/cd-dvd/iso-image.nix` sets a boot.postBootCommands which
+    # imports /nix/store/nix-path-registration, which does not exist in the 9p-
+    # based /nix/store, and then runs a nix-env command that attempts to realise
+    # a store path, which hits the network and fails. This is copied from
+    # `virtualisation/qemu-vm.nix` but with extra `lib.mkForce`.
+    boot.postBootCommands = lib.mkForce ''
+      if [[ "$(cat /proc/cmdline)" =~ regInfo=([^ ]*) ]]; then
+        ${config.nix.package.out}/bin/nix-store --load-db < ''${BASH_REMATCH[1]}
+      fi
+
+      # `qemu-vm.nix` does not wipe this device between runs, so ensure it's
+      # ready for use. (We could instead use `virtualisation.emptyDiskImages`
+      # but that puts them in $TMPDIR and the UX there is kinda bad.)
+      ${pkgs.util-linux}/bin/wipefs --all --quiet /dev/vda
+    '';
+  };
+}
