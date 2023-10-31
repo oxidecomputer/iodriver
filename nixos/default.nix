dda58b31d0f6b6f5df4a1bb9f9c9d912e289a559
iliana etaoin <iliana@oxide.computer>
2023-10-30 18:04:49 -0700

    license headers on nix code

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Sets basic NixOS options for the generated ISO, and imports of all the other
 # modules we're using.
 
