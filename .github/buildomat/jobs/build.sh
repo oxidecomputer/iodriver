88ca6a9ae1ead6536e3c44bc3221be1e9a05b782
iliana etaoin <iliana@oxide.computer>
2023-11-06 14:52:33 -0800

    ausdflkjdsf

--- .github/buildomat/jobs/build.sh
+++ .github/buildomat/jobs/build.sh
@@ -22,7 +22,8 @@ curl -fLOsS https://releases.nixos.org/nix/nix-2.18.1/install
 # https://releases.nixos.org/nix/nix-2.18.1/install.sha256
 echo "59bdd4f890c8dfdf8e530794bf6bf50392b6d109d772da0d953c50e6bebe34c1  install" | sha256sum -c
 sh install --yes --no-daemon --no-channel-add --no-modify-profile
+install -D .github/buildomat/nix.conf ~/.config/nix/nix.conf
 # shellcheck source=/dev/null
 source ~/.nix-profile/etc/profile.d/nix.sh
 
-nix --experimental-features="nix-command flakes" build
+nix build
