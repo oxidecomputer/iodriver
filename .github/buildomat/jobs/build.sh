a8ab8ee908f05e06a6a1a81389573f7e11b5aa95
iliana etaoin <iliana@oxide.computer>
2023-11-06 14:32:38 -0800

    initial buildomat script to build iodriver.iso

---
+++ .github/buildomat/jobs/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#:
+#: name = "iodriver.iso"
+#: variety = "basic"
+#: target = "ubuntu-20.04"
+#: output_rules = [
+#:	"=/work/oxidecomputer/iodriver/result/iso/iodriver.iso",
+#:	"=/nix/var/log/nix/drvs/*/*",
+#: ]
+#:
+#: [[publish]]
+#: series = "iodriver"
+#: name = "iodriver.iso"
+#: from_output = "/work/oxidecomputer/iodriver/result/iso/iodriver.iso"
+#:
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+curl -fLOsS https://releases.nixos.org/nix/nix-2.18.1/install
+# https://releases.nixos.org/nix/nix-2.18.1/install.sha256
+echo "59bdd4f890c8dfdf8e530794bf6bf50392b6d109d772da0d953c50e6bebe34c1  install" | sha256sum -c
+sh install --yes --no-daemon \
+	--no-channel-add --no-modify-profile \
+	--nix-extra-conf-file .github/buildomat/nix.conf
+
+nix build
